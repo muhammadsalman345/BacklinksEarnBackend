0c5b83c34a4f39edc292ff59001c9390c5c31eac
muhammadsalman345 <salmankhanhissam345@gmail.com>
2023-12-18 00:22:13 -0800

    first commit

---
+++ controlers/UsersControllers.js
@@ -0,0 +1,17 @@
+const User = require('../models/usermodel');
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
