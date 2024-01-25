74edbfe82869d97127aac17d0ee4e8b190b34162
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-01-25 03:37:14 -0800

    Initial commit

--- controlers/UsersControllers.js
+++ controlers/UsersControllers.js
@@ -2,13 +2,17 @@ const User = require('../models/usermodel');
 
 exports.signup = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
+    const { fullName, email, password } = req.body;
+    console.log(req.body); // Yahan console pe data dekho
+
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
-    const newUser = new User({ username, email, password });
+
+    const newUser = new User({ fullName, email, password });
     await newUser.save();
+
     res.status(201).json({ message: 'User registered successfully' });
   } catch (err) {
     console.error(err);
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./bd/dbConnection');
 const authRoutes = require('./routes/authRoutes');
 
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', authRoutes);
