b2e8e4efe7bdc255b13b5941d4bf3b8e307509ba
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-01-25 04:05:46 -0800

    Initial commit4

--- bd/dbConnection.js
+++ bd/dbConnection.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://admin:admin@cluster0.cmedzom.mongodb.net/', {
-      // Remove the deprecated options
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
+      useNewUrlParser: true,
+  useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
