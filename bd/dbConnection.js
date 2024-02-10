7fd51e7f5804d529f859779e169d0486186ef725
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-02-10 04:25:57 -0800

    sinup api 100% ok and working and also signi 100 ok

--- bd/dbConnection.js
+++ bd/dbConnection.js
@@ -1,29 +1,10 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect('mongodb+srv://admin:admin@cluster0.cmedzom.mongodb.net/', {
-//       // Remove the deprecated options
-//       // useNewUrlParser: true,
-//       // useUnifiedTopology: true,
-//     });
-//     console.log('Connected to MongoDB');
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//   }
-// };
-
-// module.exports = connectDB;
-
-
-// db/dbConnection.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://admin:admin@cluster0.cmedzom.mongodb.net/', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
