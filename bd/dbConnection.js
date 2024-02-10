05f42170aeac097f07fd83c21c6e613bdf23b556
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-02-10 05:09:34 -0800

    sinup api 100% ok and working and also signi 100 ok2

--- bd/dbConnection.js
+++ bd/dbConnection.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://admin:admin@cluster0.cmedzom.mongodb.net/', {
-      useNewUrlParser: true, // Use this line if you are using Mongoose version older than 6.x
-      useUnifiedTopology: true, // Use this line if you are using Mongoose version older than 6.x
-      useCreateIndex: true, // Use this line to enable the new index creation mechanism
-      useFindAndModify: false, // Use this line to use the new findOneAndUpdate method instead of findAndModify
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
