b38905a066498357dfa0264d86ef3af52479b9a4
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-02-10 04:31:16 -0800

    sinup api 100% ok and working and also signi 100 ok

--- bd/dbConnection.js
+++ bd/dbConnection.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://admin:admin@cluster0.cmedzom.mongodb.net/', {
-      useCreateIndex: true, // Add this line to use the new index creation mechanism
-      useFindAndModify: false, // Add this line to use the new findOneAndUpdate method instead of findAndModify
+      // Remove the deprecated options
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
@@ -15,3 +16,19 @@ const connectDB = async () => {
 module.exports = connectDB;
 
 
+// db/dbConnection.js
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect('mongodb+srv://admin:admin@cluster0.cmedzom.mongodb.net/', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('Connected to MongoDB');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//   }
+// };
+
+// module.exports = connectDB;
